7a7625278abce566a70db4c1e5158866baf06f17
Narimane Nekkab <nekkab0000@service06.cluster.bc2.ch>
2022-07-07 15:57:32 +0200

    added param table generation functions for iTPP1

---
+++ 0_scenarios/Outcomes_Reports_Jan2022/iTPP1/generate_param_table_iTPP1.R
@@ -0,0 +1,371 @@
+########################################
+# script generate_param_table.R
+#
+# construct a table with parameter values for each scenario of an OM experiment
+#
+# INPUTS:
+#   exp: experiment name
+#   param_ranges_cont: continous paramter ranges for parameters to be sampled
+#   param_cat: categorical scenario parameters
+#   noSamples: number of parameters sampled via lhs
+#   noSeeds: no. seeds per OM simulation
+#   chunk_size: batch size for simulation submission
+#
+# OUTPUTS:
+#	- "big" paramter table and chunk_size parameter tables for OM submission and scenario creation in the GROUP and experiment folder
+#
+# created 21.01.2018
+# monica.golumbeanu@unibas.ch
+#
+# modified 07.07.2022
+# narimane.nekkab@unibas.ch
+#
+########################################
+
+# Original function
+gen_paramtable <- function(exp, param_ranges_cont,param_cat, noSamples, noSeeds, chunk_size) {
+
+user <- strsplit(getwd(), "/", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]][5]
+user_dir <- paste0(getwd(),"/Experiments/",exp)
+
+GROUP = "/scicore/home/penny/GROUP/M3TPP/"
+
+ranges_file = paste0(user_dir,"/param_ranges.RData")
+cat_file = paste0(user_dir,"/params_cat.RData")
+table_file = paste0(user_dir,"/param_tab.txt")
+scaffold_file= paste0(user_dir,"/OM_jobs/scaffold.xml")
+
+# sample continuous parameter values 
+
+Xcand = as.data.frame(lhs(noSamples, param_ranges_cont))
+colnames(Xcand) = rownames(param_ranges_cont)
+
+SEED = c(1:noSeeds)
+
+# save continous parameter ranges for the experiment and copy to GROUP folder
+save(param_ranges_cont, file = ranges_file)
+
+file.copy(ranges_file, paste0(GROUP,exp,"/param_ranges.RData"),overwrite=TRUE)
+
+save(param_cat, file = cat_file)
+file.copy(cat_file, paste0(GROUP,exp,"/param_ranges_cat.RData"),overwrite=TRUE)
+
+
+# Table with the parameter values
+param_all <- param_cat
+param_all[[length(param_cat)+1]] <- as.data.frame(Xcand)
+
+param_tab_all = Reduce(merge ,param_all)
+
+
+
+param_tab = cbind(scenarios_names= paste(exp, 1:nrow(param_tab_all), sep="_"),
+                   param_tab_all)
+ colnames(param_tab)[1] = "Scenario_Name"
+ 
+# Add seed column at the beginning
+ param_tab = merge(param_tab, as.data.frame(SEED))
+ 
+ # Write table to specified destination file
+file.remove(table_file)
+ write.table(param_tab, table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+             row.names = FALSE)
+ 
+ file.copy(table_file, paste0(GROUP,exp,"/param_tab.txt"),overwrite=TRUE)
+ 
+ # copy over scaffold file 
+ file.remove(paste0(GROUP,exp,"/scaffold.xml"))
+
+ file.copy(scaffold_file, paste0(GROUP,exp,"/scaffold.xml"),overwrite=TRUE)
+
+
+
+chunk <- chunk_size
+n <- nrow(param_tab)
+r  <- rep(1:ceiling(n/chunk),each=chunk)[1:n]
+split_param_tab <- split(param_tab,r)
+
+num_tab <- max(r)
+for(j in 0:(num_tab-1)){ 
+  
+table_file = paste0(GROUP,exp,"/param_tab_",j,".txt")
+
+# Write table to specified destination file
+file.remove(table_file)
+write.table(split_param_tab[[j+1]], table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+            row.names = FALSE)
+
+}
+
+}
+
+# Simple function without sampling
+gen_paramtable_simple <- function(exp, param_cat, noSeeds, chunk_size) {
+  
+  user <- strsplit(getwd(), "/", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]][5]
+  user_dir <- paste0(getwd(),"/Experiments/",exp)
+  
+  GROUP = "/scicore/home/penny/GROUP/M3TPP/"
+  
+  # ranges_file = paste0(user_dir,"/param_ranges.RData")
+  cat_file = paste0(user_dir,"/params_cat.RData")
+  table_file = paste0(user_dir,"/param_tab.txt")
+  scaffold_file= paste0(user_dir,"/OM_jobs/scaffold.xml")
+  
+  # sample continuous parameter values 
+  # Xcand = as.data.frame(lhs(noSamples, param_ranges_cont))
+  # colnames(Xcand) = rownames(param_ranges_cont)
+  
+  SEED = c(1:noSeeds)
+  
+  # save continous parameter ranges for the experiment and copy to GROUP folder
+  # save(param_ranges_cont, file = ranges_file)
+  
+  # file.copy(ranges_file, paste0(GROUP,exp,"/param_ranges.RData"),overwrite=TRUE)
+  
+  save(param_cat, file = cat_file)
+  file.copy(cat_file, paste0(GROUP,exp,"/param_ranges_cat.RData"),overwrite=TRUE)
+  
+  
+  # # Table with the parameter values
+  param_all <- param_cat
+  # param_all[[length(param_cat)+1]] <- as.data.frame(Xcand)
+  
+  param_tab_all = Reduce(merge, param_all)
+  
+  param_tab = cbind(scenarios_names= paste(exp, 1:nrow(param_tab_all), sep="_"),
+                    param_tab_all)
+  colnames(param_tab)[1] = "Scenario_Name"
+  
+  # Add seed column at the beginning
+  param_tab = merge(param_tab, as.data.frame(SEED))
+  
+  # Write table to specified destination file
+  file.remove(table_file)
+  write.table(param_tab, table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+              row.names = FALSE)
+  
+  file.copy(table_file, paste0(GROUP,exp,"/param_tab.txt"),overwrite=TRUE)
+  
+  # copy over scaffold file 
+  file.remove(paste0(GROUP,exp,"/scaffold.xml"))
+  
+  file.copy(scaffold_file, paste0(GROUP,exp,"/scaffold.xml"),overwrite=TRUE)
+  
+  
+  
+  chunk <- chunk_size
+  n <- nrow(param_tab)
+  r  <- rep(1:ceiling(n/chunk),each=chunk)[1:n]
+  split_param_tab <- split(param_tab,r)
+  
+  num_tab <- max(r)
+  for(j in 0:(num_tab-1)){ 
+    
+    table_file = paste0(GROUP,exp,"/param_tab_",j,".txt")
+    
+    # Write table to specified destination file
+    file.remove(table_file)
+    write.table(split_param_tab[[j+1]], table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+                row.names = FALSE)
+    
+  }
+  
+}
+
+# Created by Narimane Nekkab to remove EIR, access, and seasonality combinations to reduce number of sims
+gen_paramtable_optimizedCombos <- function(exp, param_ranges_cont,param_cat, noSamples, noSeeds, chunk_size, EIR_ref) {
+  
+  user <- strsplit(getwd(), "/", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]][5]
+  user_dir <- paste0(getwd(),"/Experiments/",exp)
+  
+  GROUP = "/scicore/home/penny/GROUP/M3TPP/"
+  
+  ranges_file = paste0(user_dir,"/param_ranges.RData")
+  cat_file = paste0(user_dir,"/params_cat.RData")
+  table_file = paste0(user_dir,"/param_tab.txt")
+  scaffold_file= paste0(user_dir,"/OM_jobs/scaffold.xml")
+  
+  # sample continuous parameter values 
+  Xcand = as.data.frame(lhs(noSamples, param_ranges_cont))
+  colnames(Xcand) = rownames(param_ranges_cont)
+  SEED = c(1:noSeeds)
+  
+  # save continous parameter ranges for the experiment and copy to GROUP folder
+  save(param_ranges_cont, file = ranges_file)
+  file.copy(ranges_file, paste0(GROUP,exp,"/param_ranges.RData"),overwrite=TRUE)
+  save(param_cat, file = cat_file)
+  file.copy(cat_file, paste0(GROUP,exp,"/param_ranges_cat.RData"),overwrite=TRUE)
+  
+  # Table with the parameter values
+  param_all <- param_cat
+  param_all[[length(param_cat)+1]] <- as.data.frame(Xcand)
+  param_tab_all = Reduce(merge ,param_all)
+  
+  ############################################
+  ############# REMOVE PARAMETER ############# 
+  
+  # Column order
+  param_tab_columns = colnames(param_tab_all)
+  
+  # Seasonal levels
+  seasonal_profiles=levels(param_tab_all$Seasonality)
+  param_tab_all$Seasonality = as.character(param_tab_all$Seasonality)
+  
+  # Column from ref
+  columns_ref = colnames(EIR_ref)
+  EIR_ref$Seasonality = as.character(EIR_ref$Seasonality)
+  
+  # Match
+  matched_ref = merge(EIR_ref, param_tab_all, by = columns_ref, all.x = T)
+  
+  # Reorder columns 
+  matched_ref = matched_ref[,param_tab_columns]
+  
+  # Remove duplicates
+  matched_ref = matched_ref %>% dplyr::distinct()
+  
+  # Replace
+  param_tab_all = matched_ref
+  
+  ############################################
+  
+  param_tab = cbind(scenarios_names= paste(exp, 1:nrow(param_tab_all), sep="_"),
+                    param_tab_all)
+  colnames(param_tab)[1] = "Scenario_Name"
+  
+  # Add seed column at the beginning
+  param_tab = merge(param_tab, as.data.frame(SEED))
+  
+  # Write table to specified destination file
+  file.remove(table_file)
+  write.table(param_tab, table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+              row.names = FALSE)
+  
+  file.copy(table_file, paste0(GROUP,exp,"/param_tab.txt"),overwrite=TRUE)
+  
+  # copy over scaffold file 
+  file.remove(paste0(GROUP,exp,"/scaffold.xml"))
+  
+  file.copy(scaffold_file, paste0(GROUP,exp,"/scaffold.xml"),overwrite=TRUE)
+  
+  
+  
+  chunk <- chunk_size
+  n <- nrow(param_tab)
+  r  <- rep(1:ceiling(n/chunk),each=chunk)[1:n]
+  split_param_tab <- split(param_tab,r)
+  
+  num_tab <- max(r)
+  for(j in 0:(num_tab-1)){ 
+    
+    table_file = paste0(GROUP,exp,"/param_tab_",j,".txt")
+    
+    # Write table to specified destination file
+    file.remove(table_file)
+    write.table(split_param_tab[[j+1]], table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+                row.names = FALSE)
+    
+  }
+  
+}
+
+# Created by Narimane Nekkab to remove EIR, access, and seasonality combinations to reduce number of sims
+gen_paramtable_optimizedCombos_catonly <- function(exp, param_cat, noSamples, noSeeds, chunk_size, EIR_ref) {
+  
+  user <- strsplit(getwd(), "/", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]][5]
+  user_dir <- paste0(getwd(),"/Experiments/",exp)
+  
+  GROUP = "/scicore/home/penny/GROUP/M3TPP/"
+  
+  # ranges_file = paste0(user_dir,"/param_ranges.RData")
+  cat_file = paste0(user_dir,"/params_cat.RData")
+  table_file = paste0(user_dir,"/param_tab.txt")
+  scaffold_file= paste0(user_dir,"/OM_jobs/scaffold.xml")
+  
+  # # sample continuous parameter values 
+  # Xcand = as.data.frame(lhs(noSamples, param_ranges_cont))
+  # colnames(Xcand) = rownames(param_ranges_cont)
+  SEED = c(1:noSeeds)
+  # 
+  # # save continous parameter ranges for the experiment and copy to GROUP folder
+  # save(param_ranges_cont, file = ranges_file)
+  # file.copy(ranges_file, paste0(GROUP,exp,"/param_ranges.RData"),overwrite=TRUE)
+  # save(param_cat, file = cat_file)
+  # file.copy(cat_file, paste0(GROUP,exp,"/param_ranges_cat.RData"),overwrite=TRUE)
+  
+  # Table with the parameter values
+  param_all <- param_cat
+  # param_all[[length(param_cat)+1]] <- as.data.frame(Xcand)
+  param_tab_all = Reduce(merge, param_all)
+  
+  ############################################
+  ############# REMOVE PARAMETER ############# 
+  
+  # Column order
+  param_tab_columns = colnames(param_tab_all)
+  
+  # Seasonal levels
+  seasonal_profiles=levels(param_tab_all$Seasonality)
+  param_tab_all$Seasonality = as.character(param_tab_all$Seasonality)
+  
+  # Column from ref
+  columns_ref = colnames(EIR_ref)
+  EIR_ref$Seasonality = as.character(EIR_ref$Seasonality)
+  
+  # Match
+  matched_ref = merge(EIR_ref, param_tab_all, by = columns_ref, all.x = T)
+  
+  # Reorder columns 
+  matched_ref = matched_ref[,param_tab_columns]
+  
+  # Remove duplicates
+  matched_ref = matched_ref %>% dplyr::distinct()
+  
+  # Replace
+  param_tab_all = matched_ref
+  
+  # Remove na
+  param_tab_all = param_tab_all[complete.cases(param_tab_all),]
+  
+  ############################################
+  
+  param_tab = cbind(scenarios_names= paste(exp, 1:nrow(param_tab_all), sep="_"),
+                    param_tab_all)
+  colnames(param_tab)[1] = "Scenario_Name"
+  
+  # Add seed column at the beginning
+  param_tab = merge(param_tab, as.data.frame(SEED))
+  
+  # Write table to specified destination file
+  file.remove(table_file)
+  write.table(param_tab, table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+              row.names = FALSE)
+  
+  file.copy(table_file, paste0(GROUP,exp,"/param_tab.txt"),overwrite=TRUE)
+  
+  # copy over scaffold file 
+  file.remove(paste0(GROUP,exp,"/scaffold.xml"))
+  
+  file.copy(scaffold_file, paste0(GROUP,exp,"/scaffold.xml"),overwrite=TRUE)
+  
+  
+  
+  chunk <- chunk_size
+  n <- nrow(param_tab)
+  r  <- rep(1:ceiling(n/chunk),each=chunk)[1:n]
+  split_param_tab <- split(param_tab,r)
+  
+  num_tab <- max(r)
+  for(j in 0:(num_tab-1)){ 
+    
+    table_file = paste0(GROUP,exp,"/param_tab_",j,".txt")
+    
+    # Write table to specified destination file
+    file.remove(table_file)
+    write.table(split_param_tab[[j+1]], table_file, sep = "\t", quote = FALSE, col.names = TRUE,
+                row.names = FALSE)
+    
+  }
+  
+}
