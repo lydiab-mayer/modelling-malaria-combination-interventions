9e67ad7777590e5c304ac1527990a6165d9b81c5
Lydia Braunack-Mayer <brauna0000@login20.cluster.bc2.ch>
2024-12-02 16:26:09 +0100

    finalise trace plots with RTSS

---
+++ data_and_figures/supplement_fig13/plot_fig13.R
@@ -0,0 +1,103 @@
+############################################################
+#
+# Visualises cumulative cases per person year
+#
+# Written by Lydia Braunack-Mayer
+############################################################
+
+# ----------------------------------------------------------
+# Setup
+# ----------------------------------------------------------
+
+rm(list = ls())
+library(ggplot2)
+library(patchwork)
+library(dplyr)
+
+# Read in data
+dfOut <- readRDS("./data_and_figures/supplement_fig13/data_fig13.rds")
+dfOutAggregate <- readRDS("./data_and_figures/supplement_fig13/data_aggregate_fig13.rds")
+
+# Filter data
+dfOut <- dfOut %>%
+  filter(EIR == 4,
+         Seasonality == "seas6mo",
+         Access == 0.04)
+dfOutAggregate <- dfOutAggregate %>%
+  filter(EIR == 4,
+         Seasonality == "seas6mo",
+         Access == 0.04)
+
+# Read in plot label
+param_label <- readRDS("./data_and_figures/supplement_fig13/label_fig13.rds")
+param_label
+
+# Define colours
+cols <- c("#468AB2", "#22223B", "#EB5160", "#ffa74f", "#F4DBD8")
+
+# ----------------------------------------------------------
+# Generate plots
+# ----------------------------------------------------------
+
+p <- ggplot()
+
+p <- p + geom_rect(data = data.frame("Outcome" = unique(dfOut$Outcome)), aes(xmin = 0.25, xmax = 5, ymin = -Inf, ymax = Inf), fill = cols[5], alpha = 0.5) +
+  geom_line(data = dfOutAggregate, aes(x = AgeGroup, y = medianValue, colour = Intervention), linewidth = 0.5) +
+  geom_point(data = dfOutAggregate, aes(x = AgeGroup, y = medianValue, colour = Intervention), size = 1) +
+  geom_ribbon(data = dfOutAggregate, aes(x = AgeGroup, ymin = minValue, ymax = maxValue, fill = Intervention), alpha = 0.2, linewidth = 0.3)
+
+p <- p + facet_wrap(. ~ Outcome, scales = "free_y")
+
+p <- p + theme(panel.border = element_blank(), 
+               panel.background = element_blank(),
+               strip.background = element_blank(),
+               strip.text = element_text(family = "Times", face = "bold", size = 9),
+               panel.grid.major.y = element_line(colour = "grey80", linetype = "dotted"),
+               panel.grid.major.x = element_blank(),
+               panel.grid.minor = element_blank(),
+               axis.line = element_blank(),
+               axis.text = element_text(family = "Times", colour = "grey45", margin = margin(t = 5)),
+               axis.title = element_text(family = "Times", colour = "grey30", face="bold", size = 8), 
+               legend.position = "bottom",
+               legend.key = element_rect(fill = NA),
+               legend.text = element_text(family = "Times"),
+               title = element_text(family = "Times", face = "bold", size = 9),
+               plot.title.position = "plot")
+
+p <- p + scale_colour_manual(values = cols) +
+  scale_fill_manual(values = cols, guide = "none") +
+  scale_x_continuous(limits = c(0, 10),
+                     breaks = seq(0, 10, 1))
+
+p <- p + labs(x = "Age (years)",
+              y = "Cumulative cases per person",
+              colour = "")
+
+# ----------------------------------------------------------
+# Save plots
+# ----------------------------------------------------------
+
+p
+
+ggsave(filename = paste0("./data_and_figures/supplement_fig13/fig13.jpg"),
+       plot = last_plot(),
+       width = 8,
+       height = 5.5,
+       dpi = 400)
+
+# ----------------------------------------------------------
+# Calculate point estimates for manuscript text
+# ----------------------------------------------------------
+
+# Extract cumulative cases at 5 and 10 years
+pointEstimates <- dfOutAggregate %>%
+  filter(Intervention %in% c("SMC", "Pre-liver stage therapeutic", "SMC + pre-liver stage therapeutic"),
+         AgeGroup %in% c(5, 10)) %>%
+  select(-c(minValue, maxValue)) %>%
+  pivot_wider(names_from = Intervention,
+              values_from = medianValue)
+
+# Calculate impact relative to each counterfactual scenario
+pointEstimates <- pointEstimates %>%
+  mutate(impactSMC = (SMC - `SMC + pre-liver stage therapeutic`) / SMC,
+         impactLiverStage = (`Pre-liver stage therapeutic` - `SMC + pre-liver stage therapeutic`) / `Pre-liver stage therapeutic`)
